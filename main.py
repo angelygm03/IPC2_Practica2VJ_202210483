f49af06745c571f72134dcc5277d30586ed2ae49
angely.gm03 <gmartinez.angely@gmail.com>
2024-06-26 23:12:12 -0600

    libro por id y por categoria

--- main.py
+++ main.py
@@ -24,13 +24,30 @@ def cargarLibros():
         copias = int(elemento.find('copias').text)
 
         manager.addLibro(id, titulo, autor, idioma, categoria, editorial, copias)
-            
+
     return jsonify({"message": "Archivo XML cargado correctamente"}), 200
 
 @app.route('/verLibros', methods=['GET'])
 def verLibros():
     libros = manager.getLibro()
     return jsonify(libros), 200
+
+@app.route('/verLibro/:<id>', methods=['GET'])
+def crearXML(id):
+    xml_str = manager.createXMLFromID(id)
+    if xml_str:
+        return xml_str, 200, {'Content-Type': 'application/xml'}
+    else:
+        return jsonify({"message": "Libro no encontrado"}), 404
+
+
+@app.route('/libros/:<categoria>', methods=['GET'])
+def verLibroCategoria(categoria):
+    libros = manager.getLibrosCategoria(categoria)
+    if not libros:
+        return jsonify({"message": "Libros no encontrados"}), 404
+    return jsonify(libros), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree as ET
+from xml.dom.minidom import parseString
 from libro import Libro
 
 class Manager():
@@ -22,3 +24,44 @@ class Manager():
             }
             json.append(libro)
         return json
+    
+    def getLibroId(self, id):
+        for libro in self.libros:
+            if libro.id == id:
+                return libro
+        return None
+    
+    def getLibrosCategoria(self, categoria):
+        libros_categoria = [libro for libro in self.libros if libro.categoria == categoria]
+        json = []
+        for libro in libros_categoria:
+            libro_json = {
+                'id': libro.id,
+                'titulo': libro.titulo,
+                'autor': libro.autor,
+                'idioma': libro.idioma,
+                'categoria': libro.categoria,
+                'editorial': libro.editorial,
+                'copias': libro.copias
+            }
+            json.append(libro_json)
+        return json
+
+
+    def createXMLFromID(self, id):
+        for libro in self.libros:
+            if libro.id == id:
+                root = ET.Element('libro', attrib={'id': libro.id})
+                ET.SubElement(root, 'titulo').text = libro.titulo
+                ET.SubElement(root, 'autor').text = libro.autor
+                ET.SubElement(root, 'idioma').text = libro.idioma
+                ET.SubElement(root, 'categoria').text = libro.categoria
+                ET.SubElement(root, 'editorial').text = libro.editorial
+                ET.SubElement(root, 'copias').text = str(libro.copias)
+                
+                tree = ET.ElementTree(root)
+                xml_str = ET.tostring(root, encoding='utf-8')
+                pretty_xml_str = parseString(xml_str).toprettyxml(indent="  ", encoding='utf-8')
+                
+                return pretty_xml_str.decode('utf-8')
+        return None
